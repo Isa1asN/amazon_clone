c01e4f61ddf7331d963e7c8453bc87737dd0cc79
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-07-22 22:27:22 +0300

    added the basic home page

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,13 +1,20 @@
+import React from 'react';
 import './App.css';
-import './header/Header.js'
+import './header/Header.js';
 import Header from './header/Header.js';
 import Home from './home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-    <Header />
-    <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={[<Header />, <Home /> ]} />
+          <Route path="/checkout" element={[<Header />, <h1>The checkout page</h1>]} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
