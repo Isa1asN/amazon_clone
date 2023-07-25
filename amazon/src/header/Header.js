ee985e430303a47bd80e2d4a8e028a7ab50df631
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-07-25 12:22:32 +0300

    added context api

--- amazon/src/header/Header.js
+++ amazon/src/header/Header.js
@@ -3,8 +3,11 @@ import './header.css';
 import SearchIcon from '@material-ui/icons/Search'; 
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { Link } from 'react-router-dom';
+import { useStateValue } from '../stateProvider';
 
 function Header() {
+  const [state, dispatch] = useStateValue();
+  const {cart} = state;
   return ( 
     <div className='header'>
       <Link to="/">
@@ -30,7 +33,7 @@ function Header() {
         <Link to="/checkout">
           <div className='header_basket'>
             <ShoppingCartIcon className='cart_icon' />
-            <span className='header_optionLn2 header_basket_count'>0</span>
+            <span className='header_optionLn2 header_basket_count'>{cart?.length}</span>
           </div>
         </Link>
       </div>
--- amazon/src/product/Product.js
+++ amazon/src/product/Product.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './product.css';
 import StarRateIcon from '@material-ui/icons/StarRate';
+import { useStateValue } from '../stateProvider';
 
 function Product({id, title, image, price, rating}) {
+  const [state, dispatch] = useStateValue();
+  // const {cart} = state;
+  // console.log(cart)
+  const addToCart = () => {
+    dispatch({
+      type: 'ADD_TO_CART',
+      item: {
+        id : id,
+        title : title,
+        image : image,
+        price : price,
+        rating : rating
+      }
+    });
+  }
   return (
     <div className='product'>
       <div className='product_info'>
@@ -16,7 +32,7 @@ function Product({id, title, image, price, rating}) {
         </div>
       </div>
       <img src={image} alt='product_image'></img>
-      <button className='product_add_to_cart'>Add to cart</button>
+      <button className='product_add_to_cart' onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
