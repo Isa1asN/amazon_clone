16b8ed5b0e5bc311eb3a1c97a48236cc2276f5b7
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-07-26 14:00:47 +0300

    added the remove from cart reducer

--- amazon/src/reducer.js
+++ amazon/src/reducer.js
@@ -9,6 +9,15 @@ const reducer = (state, action) => {
                 ...state,
                 cart : [...state.cart, action.item ],
             };
+
+        case 'REMOVE_FROM_CART':
+            const index = state.cart.findIndex((item) => item.id === action.item.id)
+            const updatedCart = [...state.cart];
+            updatedCart.splice(index, 1)
+            return {
+                ...state,
+                cart : updatedCart,
+            };
             
         default:
             return state;
