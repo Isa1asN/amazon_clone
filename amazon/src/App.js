d6664616720278b52f9894e8d20577fdc61be606
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-07-26 16:13:53 +0300

    added a footer

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -5,6 +5,7 @@ import Header from './header/Header.js';
 import Home from './home/Home';
 import Checkout from './checkout/Checkout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './footer/Footer';
 
 function App() {
   return (
@@ -15,8 +16,8 @@ function App() {
           <Route path="/" element={[ <Home /> ]} />
           <Route path="/checkout" element={[ <Checkout />]} />
           <Route path="*" element={[<h1>:( Oops... Page does not exist! </h1>]} />
-
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
---
+++ amazon/src/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+function Footer() {
+  return (
+    <div id='footer'>
+        <p>
+            Cloned by esu, 2023
+        </p>
+    </div>
+  )
+}
+
+export default Footer
